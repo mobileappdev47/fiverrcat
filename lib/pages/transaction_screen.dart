428f51457278b54b9614299fedf918cb2f4c6c42
Ravi Patel <88827091+mobileappdev47@users.noreply.github.com>
2024-04-26 22:50:15 -0700

    income and expense screen issue

--- lib/pages/transaction_screen.dart
+++ lib/pages/transaction_screen.dart
@@ -8,6 +8,7 @@ import 'package:pokercat/addexpense/db/models/category/category_model_db.dart';
 import 'package:pokercat/addexpense/db/models/transactions/transaction_model_db.dart';
 import 'package:pokercat/addexpense/widget/transaction_helper.dart';
 import 'package:pokercat/constant.dart';
+import 'package:pokercat/imports.dart';
 import '../global/component/appbar.dart';
 
 // ignore: must_be_immutable
@@ -22,98 +23,22 @@ class _TransactionScreenState extends State<TransactionScreen> {
   NumberFormat formatter = NumberFormat('#,##0');
   DateTimeRange selectedDate = SelectDate().currentDateForCalenderSelection();
 
-  List<String> months = [
-
-  ];
-
-  List<int> years = [];
-
-
-
- getMonthNames() {
-
-    DateTime currentMonth = DateTime.now();
-
-    // Add nine months before the current month
-    for (int i = 9; i > 0; i--) {
-      DateTime month = DateTime(currentMonth.year, currentMonth.month - i);
-      String monthName = DateFormat.MMMM().format(month);
-      months.add(monthName);
-    }
-
-    // Add the current month
-    String currentMonthName = DateFormat.MMMM().format(currentMonth);
-    months.add(currentMonthName);
-
-    // Add nine months after the current month
-    for (int i = 1; i <= 9; i++) {
-      DateTime month = DateTime(currentMonth.year, currentMonth.month + i);
-      String monthName = DateFormat.MMMM().format(month);
-      months.add(monthName);
-    }
-
-print(months);
-  }
-
-  getYears() {
-
-    DateTime currentMonth = DateTime.now();
-    int currentYear = currentMonth.year;
-
-    // Add nine years before the current year
-    for (int i = 9; i > 0; i--) {
-      int year = currentYear - i;
-      if (!years.contains(year)) {
-        years.add(year);
-      }
-    }
-
-    // Add the current year if not already added
-    if (!years.contains(currentYear)) {
-      years.add(currentYear);
-    }
-
-    // Add nine years after the current year
-    for (int i = 1; i <= 9; i++) {
-      int year = currentYear + i;
-      if (!years.contains(year)) {
-        years.add(year);
-      }
-    }
-
-   print(years);
-  }
-
-
-
-
   @override
   void initState() {
- years.clear();
- months.clear();
-    getYears();
-   getMonthNames();
     years.forEach((element) {
-      if(element == DateTime.now().year)
-        {
-          if(years.indexOf(element) ==0)
-            {
-              indexAll = DateTime.now().month-1;
-            }
-          else if(years.indexOf(element) ==1)
-          {
-            indexAll =( DateTime.now().month-1) +12;
-          }
-          else
-            {
-              indexAll = (DateTime.now().month-1 )+24;
-            }
+      if (element == DateTime.now().year) {
+        if (years.indexOf(element) == 0) {
+          indexAll = DateTime.now().month - 1;
+        } else if (years.indexOf(element) == 1) {
+          indexAll = (DateTime.now().month - 1) + 6;
+        } else {
+          indexAll = (DateTime.now().month - 1) + 12;
         }
+      }
     });
-    selectedDate = SelectDate()
-        .selectMonth(DateTime.now().month, DateTime.now().year);
-    TransactionDB.instance.filterForHome(
-        selectedDate.start, selectedDate.end);
+    selectedDate =
+        SelectDate().selectMonth(DateTime.now().month, DateTime.now().year);
+    TransactionDB.instance.filterForHome(selectedDate.start, selectedDate.end);
     TransactionDB.instance.refresh();
     TransactionDB.instance.getTransactionsForCurrentMonth();
     CategoryDB.instance.getAllCategory();
@@ -123,12 +48,29 @@ print(months);
   ValueNotifier<double> incomeCustomDateNotifier = ValueNotifier(0);
   ValueNotifier<double> expenseCustomDateNotifier = ValueNotifier(0);
   ValueNotifier<double> totalCustomDateNotifier = ValueNotifier(0);
-  PageController scrollController = PageController();
+
+  PageController scrollController =
+      PageController(initialPage: DateTime.now().month);
   int indexAll = DateTime.now().month;
+  List<String> months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ];
+
+  List<int> years = [2023, 2024, 2025];
 
   @override
   Widget build(BuildContext context) {
-
     // List<String> months = List.generate(12, (index) {
     //   return DateFormat('MMMM').format(DateTime(2024, index + 1, 1));
     // });
@@ -153,58 +95,182 @@ print(months);
       ),
       body: Column(
         children: [
-
+          // Padding(
+          //   padding: const EdgeInsets.all(8.0),
+          //   child: Row(
+          //     children: [
+          //       Row(
+          //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          //         children: [
+          //           //--------------------------------------------왼쪽 뾰족이 버튼---------------------------------------
+          //           IconButton(
+          //             alignment: Alignment.centerRight,
+          //             icon: const Icon(Icons.arrow_back_ios,
+          //                 color: AppTheme.pcTextSecondayColor, size: 16),
+          //             onPressed: () {
+          //               setState(
+          //                 () {
+          //                   selectedDate = SelectDate().selectePreviousMonth();
+          //                 },
+          //               );
+          //               TransactionDB.instance.filterForHome(
+          //                   selectedDate.start, selectedDate.end);
+          //               TransactionDB.instance.getTransactionsForCurrentMonth();
+          //             },
+          //           ),
+          //           //--------------------------------------------가운데 날짜 텍스트 버튼---------------------------------------
+          //           TextButton(
+          //             style: TextButton.styleFrom(
+          //                 foregroundColor: AppTheme.pcScafoldColor),
+          //             onPressed: () async {
+          //               var daterange =
+          //                   SelectDate().currentDateForCalenderSelection();
+          //               DateTimeRange? picked = await showDateRangePicker(
+          //                   context: context,
+          //                   builder: (context, child) {
+          //                     return Theme(
+          //                       data: ThemeData.dark().copyWith(
+          //                           colorScheme: const ColorScheme.dark(
+          //                               onPrimary: AppTheme.pcAppBarColor,
+          //                               onSurface: AppTheme.pcTextSecondayColor,
+          //                               primary: AppTheme.pcTextTertiaryColor),
+          //                           dialogBackgroundColor:
+          //                               AppTheme.pcAppBarColor),
+          //                       child: child!,
+          //                     );
+          //                   },
+          //                   firstDate: DateTime(DateTime.now().year - 1),
+          //                   lastDate: DateTime.now(),
+          //                   initialDateRange: daterange);
+          //               if (picked != null) {
+          //                 CategoryDB.instance.getAllCategory();
+          //                 TransactionDB.instance
+          //                     .filterForHome(picked.start, picked.end);
+          //                 setState(() {
+          //                   selectedDate = picked;
+          //                 });
+          //               }
+          //               TransactionDB.instance.getTransactionsForCurrentMonth();
+          //             },
+          //             child: Text(
+          //               "${DateFormat('MM.dd').format(selectedDate.start)} - ${DateFormat('MM.dd').format(selectedDate.end)}",
+          //               style: TextStyle(
+          //                   fontSize: fontSize,
+          //                   fontWeight: FontWeight.bold,
+          //                   color: AppTheme.pcTextSecondayColor),
+          //             ),
+          //           ),
+          //           //--------------------------------------------오른쪽 뾰족이 버튼---------------------------------------
+          //           IconButton(
+          //             alignment: Alignment.centerLeft,
+          //             icon: const Icon(Icons.arrow_forward_ios,
+          //                 color: AppTheme.pcTextSecondayColor, size: 16),
+          //             onPressed: () {
+          //               setState(
+          //                 () {
+          //                   selectedDate = SelectDate().selecteNextMonth();
+          //                 },
+          //               );
+          //               TransactionDB.instance.refresh();
+          //             },
+          //           ),
+          //         ],
+          //       ),
+          //       const Spacer(
+          //         flex: 1,
+          //       ),
+          //     ],
+          //   ),
+          // ),
           SizedBox(
             height: 80,
             child: Row(
               children: [
-
+                // IconButton(
+                //   alignment: Alignment.centerRight,
+                //   icon: const Icon(Icons.arrow_back_ios,
+                //       color: AppTheme.pcTextSecondayColor, size: 16),
+                //   onPressed: () {
+                //     setState(
+                //       () {
+                //         if(scrollController.page!.round() >0) {
+                //
+                //           selectedDate = SelectDate().selectMonth(scrollController.page!.round() - 1,
+                //               DateTime
+                //                   .now()
+                //                   .year);
+                //         }
+                //
+                //       },
+                //     );
+                //     TransactionDB.instance
+                //         .filterForHome(selectedDate.start, selectedDate.end);
+                //     TransactionDB.instance.getTransactionsForCurrentMonth();
+                //   },
+                // ),
                 Expanded(
                   child: ListView.builder(
                     controller: scrollController,
-                    itemCount: months.length * years.length,
+                    itemCount: 12, // Fixed to 12 months
                     scrollDirection: Axis.horizontal,
                     itemBuilder: (BuildContext context, int index) {
-                      int monthIndex = index % months.length;
-                      int yearIndex = index ~/ months.length;
-                      int year = years[yearIndex];
-                      String month = months[monthIndex];
+                      int currentMonth = DateTime.now().month;
+                      int currentYear = DateTime.now().year;
+
+                      int displayMonth = currentMonth - 9 + index;
+                      int displayYear = currentYear;
+
+                      if (displayMonth <= 0) {
+                        displayMonth += 12;
+                        displayYear -= 1;
+                      } else if (displayMonth > 12) {
+                        displayMonth -= 12;
+                        displayYear += 1;
+                      }
+
+                      String month = months[displayMonth - 1]; // Adjust index
+
+                      if (displayYear < 2023 || (displayYear == 2025 && displayMonth > 1)) {
+                        return const SizedBox();
+                      }
+
                       return Row(
                         children: [
                           Padding(
                             padding: const EdgeInsets.all(8.0),
                             child: InkWell(
                               onTap: () {
-                                if (year != 2025) {
+                                if (displayYear <= 2024) {
                                   setState(() {
                                     indexAll = index;
-                                    selectedDate = SelectDate().selectMonth(monthIndex + 1, year);
+                                    selectedDate = SelectDate().selectMonth(displayMonth, displayYear);
                                     TransactionDB.instance.filterForHome(selectedDate.start, selectedDate.end);
                                     TransactionDB.instance.getTransactionsForCurrentMonth();
                                   });
                                 }
-
                               },
                               child: Column(
                                 children: [
                                   Text(
                                     month,
-                                    // months[index],
                                     style: const TextStyle(color: Colors.white),
                                   ),
-                                  Text(
-                                    '${year == 2024 ? '' : year}',
-                                    style: const TextStyle(color: Colors.white),
+                                  Visibility(
+                                    visible: displayYear != 2024,
+                                    child: Text(
+                                      '$displayYear',
+                                      style: const TextStyle(color: Colors.white),
+                                    ),
                                   ),
                                   const SizedBox(
                                     height: 5,
                                   ),
                                   indexAll == index
                                       ? Container(
-                                          height: 1,
-                                          width: 50,
-                                          color: Colors.white,
-                                        )
+                                    height: 1,
+                                    width: 50,
+                                    color: Colors.white,
+                                  )
                                       : const SizedBox()
                                 ],
                               ),
@@ -216,6 +282,26 @@ print(months);
                   ),
                 ),
 
+
+
+                // IconButton(
+                //   alignment: Alignment.centerLeft,
+                //   icon: const Icon(Icons.arrow_forward_ios,
+                //       color: AppTheme.pcTextSecondayColor, size: 16),
+                //   onPressed: () {
+                //     setState(
+                //       () {
+                //         if(scrollController.page!.round() <12) {
+                //           selectedDate = SelectDate().selectMonth(scrollController.page!.round() + 1,
+                //               DateTime
+                //                   .now()
+                //                   .year);
+                //         }
+                //       },
+                //     );
+                //     TransactionDB.instance.refresh();
+                //   },
+                // ),
               ],
             ),
           ),
@@ -467,8 +553,74 @@ print(months);
             },
           ),
           // ----------------------------------- Transaction History End----------------------------------------
+
         ],
       ),
     );
   }
 }
+
+// Expanded(
+// child: ListView.builder(
+// controller: scrollController,
+// itemCount: 6 * years.length,
+// scrollDirection: Axis.horizontal,
+// itemBuilder: (BuildContext context, int index) {
+// int currentYear = DateTime.now().year;
+// int currentMonth = DateTime.now().month;
+// int monthIndex = (currentMonth - 9 + index) % 12;
+// int yearIndex = (currentMonth - 9 + index) ~/ 12;
+// int year = currentYear + yearIndex;
+// String month = months[monthIndex];
+// return Row(
+// children: [
+// Padding(
+// padding: const EdgeInsets.all(8.0),
+// child: InkWell(
+// onTap: () {
+// if (year <= 2024) {
+// setState(() {
+// indexAll = index;
+// selectedDate = SelectDate()
+//     .selectMonth(monthIndex + 1, year);
+// TransactionDB.instance.filterForHome(
+// selectedDate.start, selectedDate.end);
+// TransactionDB.instance
+//     .getTransactionsForCurrentMonth();
+// });
+// }
+// },
+// child: Column(
+// children: [
+// Text(
+// month,
+// // months[index],
+// style: const TextStyle(color: Colors.white),
+// ),
+// Visibility(
+// visible: year != 2024,
+// child: Text(
+// '$year',
+// style:
+// const TextStyle(color: Colors.white),
+// ),
+// ),
+// const SizedBox(
+// height: 5,
+// ),
+// indexAll == index
+// ? Container(
+// height: 1,
+// width: 50,
+// color: Colors.white,
+// )
+//     : const SizedBox()
+// ],
+// ),
+// ),
+// ),
+// ],
+// );
+// },
+// ),
+// ),
